2fe73a0cff993aa3fabe6d9999e009f341000424
E1psycongr00 <39326175+E1psycongr00@users.noreply.github.com>
2022-11-11 22:33:09 +0900

    feat: modifier 에 따라 json serialize 변환 여부를 결정하도록 수정

--- src/main/java/fields/json/JsonWriter.java
+++ src/main/java/fields/json/JsonWriter.java
@@ -1,57 +1,67 @@
 package fields.json;
 
-import java.lang.reflect.Field;
+import java.lang.reflect.*;
 
 public class JsonWriter {
 
+    /**
+     * Serializes the provided instance into a JSON String representation
+     * Update this method as per instructions.
+     * Do not change the method's signature.
+     */
     public static String objectToJson(Object instance) throws IllegalAccessException {
         Field[] fields = instance.getClass().getDeclaredFields();
         StringBuilder stringBuilder = new StringBuilder();
 
         stringBuilder.append("{");
 
-        for (int i = 0; i < fields.length; ++i) {
+        for (int i = 0; i < fields.length; i++) {
             Field field = fields[i];
             field.setAccessible(true);
+
+            int modifier = field.getModifiers();
+            if (Modifier.isStatic(modifier) || Modifier.isTransient(modifier)) {
+                continue;
+            }
 
             if (field.isSynthetic()) {
                 continue;
             }
 
             stringBuilder.append(formatStringValue(field.getName()));
+
             stringBuilder.append(":");
 
             if (field.getType().isPrimitive()) {
                 stringBuilder.append(formatPrimitiveValue(field, instance));
             } else if (field.getType().equals(String.class)) {
                 stringBuilder.append(formatStringValue(field.get(instance).toString()));
-            } else {
-                stringBuilder.append(objectToJson(field.get(instance)));
             }
 
-            if (i != fields.length -1) {
+            if (i != fields.length - 1) {
                 stringBuilder.append(",");
             }
         }
+
         stringBuilder.append("}");
         return stringBuilder.toString();
     }
 
     private static String formatPrimitiveValue(Field field, Object parentInstance) throws IllegalAccessException {
-        Class<?> type = field.getType();
-        if (type.equals(boolean.class)
-                || type.equals(int.class)
-                || type.equals(long.class)
-                || type.equals(short.class)) {
+        if (field.getType().equals(boolean.class)
+            || field.getType().equals(int.class)
+            || field.getType().equals(long.class)
+            || field.getType().equals(short.class)) {
             return field.get(parentInstance).toString();
+        } else if (field.getType().equals(double.class) || field.getType().equals(float.class)) {
+            return String.format("%.02f", field.get(parentInstance));
         }
-        if (type.equals(double.class) || type.equals(float.class)) {
-            return String.format("%.2f", field.get(parentInstance));
-        }
-        throw new RuntimeException(String.format("Type: %s %s is unsupported", type.getName()));
+
+        throw new RuntimeException(String.format("Type : %s is unsupported", field.getType().getName()));
     }
 
     private static String formatStringValue(String value) {
         return String.format("\"%s\"", value);
     }
+
 }
